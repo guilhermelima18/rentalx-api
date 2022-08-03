9d13f93107bc1fecbf8a2c689139d170a629b2b6
guilhermelima18 <guilhermelima18@hotmail.com>
2022-08-02 23:14:07 -0300

    feat: create repositories, services and model for categories

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,44 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  listById(id: string) {
+    const category = this.categories.filter((category) => category.id === id);
+    return category;
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+
+  findByName(name: string) {
+    const category = this.categories.find(
+      (category) => category.name.toLowerCase() === name.toLowerCase()
+    );
+    return category;
+  }
+}
+
+export { CategoriesRepository };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,32 @@
 import { Router, Request, Response } from "express";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
+const categoryRepository = new CategoriesRepository();
 
-const categories = [];
+categoriesRoutes.get("/categories", (request: Request, response: Response) => {
+  const allList = categoryRepository.list();
+
+  return response.status(200).json(allList);
+});
+
+categoriesRoutes.get(
+  "/categories/:id",
+  (request: Request, response: Response) => {
+    const { id } = request.params;
+    const itemList = categoryRepository.listById(id);
+
+    return response.status(200).json(itemList);
+  }
+);
 
 categoriesRoutes.post("/categories", (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  categories.push({
-    name,
-    description,
-  });
+  const createCategoryService = new CreateCategoryService(categoryRepository);
+
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
